954c1ed7aaba0ff8ddeb642b5d51161f3ae9302d
20201287강민혁 <onueer@gmail.com>
2021-06-08 14:13:42 +0900

    스테이지1~보스스테이지 맵

---
+++ Hunter_Or_Witch/Assets/Skripts/StageEnum.cs
@@ -0,0 +1,12 @@
+﻿public class StageEnum
+{
+    public enum StageName
+    {
+        stage_1, stage_2, stage_3,
+        stage_4, stage_5, stage_6,
+        stage_7, stage_8, stage_9,
+        stage_10, stage_11, stage_12,
+        stage_13, stage_14, stage_15,
+        stage_16, stage_17, stage_final
+    };
+}
